83cb4431192e65b0baa4dc0212e0571ff9f77d94
Vaibhav Salunke <89504443+semi-colonel@users.noreply.github.com>
2026-09-18 16:41:16 +0530

    chore: switch to adapter-static for GitHub Pages

--- vite.config.ts
+++ vite.config.ts
@@ -1,6 +1,5 @@
 import tailwindcss from '@tailwindcss/vite';
-// import adapter from '@sveltejs/adapter-auto';
-import adapter from '@sveltejs/adapter-cloudflare';
+import adapter from '@sveltejs/adapter-static';
 import { sveltekit } from '@sveltejs/kit/vite';
 import { defineConfig } from 'vite';
 
@@ -9,14 +8,21 @@ export default defineConfig({
 		tailwindcss(),
 		sveltekit({
 			compilerOptions: {
-				// Force runes mode for the project, except for libraries. Can be removed in svelte 6.
 				runes: ({ filename }) => filename.split(/[/\\]/).includes('node_modules') ? undefined : true
 			},
 
-			// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
-			// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
-			// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-			adapter: adapter()
+			kit: {
+				adapter: adapter({
+					pages: 'build',
+					assets: 'build',
+					fallback: '404.html',
+					precompress: false,
+					strict: true
+				}),
+				paths: {
+					base: process.env.NODE_ENV === 'production' ? '/svelte-profile' : ''
+				}
+			}
 		})
 	]
 });
